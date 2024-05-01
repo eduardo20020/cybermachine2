ed4908019dd7be5e8a20d50a42388845b00208a8
Brandon <brandonde2002@gmail.com>
2024-05-01 17:58:33 -0600

    dd

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="bg-gray-800 py-4">
+    <div class="container mx-auto px-4">
+      <div class="flex justify-between items-center">
+        <!-- Logo -->
+        <a href="#" class="text-white text-2xl font-bold">Mi Sitio</a>
+        <!-- Menú de navegación -->
+        <ul class="flex space-x-4">
+          <li><a href="#" class="text-white hover:text-gray-300 px-4">Inicio</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300 px-4">Acerca</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300 px-4">Servicios</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300 px-4">Contacto</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
